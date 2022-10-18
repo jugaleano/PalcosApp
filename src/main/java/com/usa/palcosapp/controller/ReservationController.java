67a7ce61f783b02a722284b067a100d8e02fa42d
jugaleano <juliana.galeano.mt@usa.edu.co>
2022-10-17 20:28:06 -0500

    version dos

--- src/main/java/com/usa/palcosapp/controller/ReservationController.java
+++ src/main/java/com/usa/palcosapp/controller/ReservationController.java
@@ -1,6 +1,8 @@
 package com.usa.palcosapp.controller;
 
+import com.usa.palcosapp.model.ClientReport;
 import com.usa.palcosapp.model.Reservation;
+import com.usa.palcosapp.model.ReservationReport;
 import com.usa.palcosapp.service.ReservationService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -17,26 +19,44 @@ public class ReservationController {
     private ReservationService reservationService;
 
     @GetMapping("/all")
-    public List<Reservation> getAll(){
+    public List<Reservation> getAll() {
         return reservationService.getAll();
     }
+
     @GetMapping("/{idReservation}")
-    public Optional<Reservation> getById(@PathVariable("idReservation") Integer id){
+    public Optional<Reservation> getById(@PathVariable("idReservation") Integer id) {
         return reservationService.getById(id);
     }
+
     @PostMapping("/save")
     @ResponseStatus(HttpStatus.CREATED)
-    public Reservation save(@RequestBody Reservation reservation){
+    public Reservation save(@RequestBody Reservation reservation) {
         return reservationService.save(reservation);
     }
+
     @PutMapping("/update")
     @ResponseStatus(HttpStatus.CREATED)
-    public Reservation update(@RequestBody Reservation reservation){
+    public Reservation update(@RequestBody Reservation reservation) {
         return reservationService.update(reservation);
     }
+
     @DeleteMapping("/{id}")
     @ResponseStatus(HttpStatus.NO_CONTENT)
-    public boolean delete(@PathVariable("id")Integer id){
+    public boolean delete(@PathVariable("id") Integer id) {
         return reservationService.delete(id);
+    }
+
+
+    @GetMapping("/report-status")
+    public ReservationReport getReservationStatusReport(){
+        return reservationService.getReservationReport();
+    }
+    @GetMapping("/report-dates/{dateOne}/{dateTwo}")
+    public List<Reservation> getReservationReportDates(@PathVariable("dateOne")String dateOne, @PathVariable("dateTwo")String dateTwo){
+        return reservationService.getReservationPeriod(dateOne,dateTwo);
+    }
+    @GetMapping("/report-clients")
+    public List<ClientReport> getReportClients(){
+        return reservationService.getTopClients();
     }
 }
--- src/main/java/com/usa/palcosapp/service/ReservationService.java
+++ src/main/java/com/usa/palcosapp/service/ReservationService.java
@@ -1,10 +1,17 @@
 package com.usa.palcosapp.service;
 
+import com.usa.palcosapp.model.ClientReport;
 import com.usa.palcosapp.model.Reservation;
+import com.usa.palcosapp.model.ReservationReport;
 import com.usa.palcosapp.repository.ReservationRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 import java.util.Optional;
 
@@ -64,4 +71,34 @@ public class ReservationService {
         }).orElse(false);
         return aBoolean;
     }
-}
+
+    public ReservationReport getReservationReport(){
+        List<Reservation> completed = reservationRepository.getReservationByStatus("completed");
+        List<Reservation> cancelled = reservationRepository.getReservationByStatus("cancelled");
+        return new ReservationReport(completed.size(),cancelled.size());
+    }
+
+    public List<Reservation> getReservationPeriod(String dateA, String dateB){
+        SimpleDateFormat parser = new SimpleDateFormat("yyyy-MM-dd");
+        Date aDate = new Date();
+        Date bDate = new Date();
+
+        try {
+            aDate = parser.parse(dateA);
+            bDate = parser.parse(dateB);
+        }catch(ParseException evt){
+            evt.printStackTrace();
+        }
+        if(aDate.before(bDate)){
+            return reservationRepository.getReservationPeriod(aDate, bDate);
+        }else{
+            return new ArrayList<>();
+        }
+    }
+
+    public List<ClientReport> getTopClients(){
+        return reservationRepository.getTopClient();
+    }
+    //public ReservationReport reservationReportgetReservationStatusReport(){
+       // List<Reservation> getReservationReportDates(@PathVariable("dateOne") Date dateOne, @PathVariable("dateTwo") Date dateTwo)//
+    }
